033ce482efca1cd467ef4842a9600b0074d49173
Benjamin Favre <ben@webdesign29.net>
2023-07-25 17:03:50 +0200

    update

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import app from './app'
+import { getFreePort } from './helpers'
+if (!process.env.PORT) {
+    process.env.PORT = await getFreePort(3000) as unknown as string        
+}
+
+console.log('Server running on port', process.env.PORT)
+
+export default {
+    fetch: app.fetch,
+    port: process.env.PORT,
+}
